14e1321e6033d65c3c1407eae4b11f865283d0fa
Anoosha Clarance <69040618+stepin104881@users.noreply.github.com>
2020-10-08 22:32:39 +0530

    Add files via upload

--- 3_Implementation/inc/medical.h
+++ 3_Implementation/inc/medical.h
@@ -7,11 +7,15 @@ void StockOfMedicine(int number);
 void KnowInfoAboutMedicine(int number);
 void DeleteMedicineStore(int number);
 void ChangeMedicineDetails(int number);
+void menu();
 
 struct Medicine{
    int id,price,quantity;
    char medicneName[100],Company[100],Mfg_Date[11],Exp_Date[11],info[5000];
   }m[100];
 void AddMedicineinStore(int number,struct Medicine m[]);
+
+
+
 
 #endif // MEDICAL_H_INCLUDED
